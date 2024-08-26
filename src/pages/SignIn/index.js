5cc56f5c9bf5ecfa4c705bd5cbd84d81c5295eb7
MHPontes <mateushspontes@gmail.com>
2024-08-26 08:01:15 -0300

    criando pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,38 @@
+import "./signin.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+
 export default function SignIn() {
-    return (
-        <div className="SignIn">
-        <h1>Sign In</h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
         </div>
-    );
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="***********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit"> Acessar</button>
+        </form>
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
